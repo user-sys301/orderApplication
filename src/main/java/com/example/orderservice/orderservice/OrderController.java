4a8b5176354aa772e5823bfada576970b27ad41f
Miruthula <miruthulajayaprakasham@gmail.com>
2025-09-20 03:45:14 +0530

    changes

--- src/main/java/com/example/orderservice/orderservice/OrderController.java
+++ src/main/java/com/example/orderservice/orderservice/OrderController.java
@@ -2,7 +2,7 @@ package com.example.orderservice.orderservice;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
-public class DemoController {
+public class OrderController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello, World!";
